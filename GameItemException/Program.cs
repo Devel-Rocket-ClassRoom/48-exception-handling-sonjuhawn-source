5a9323c01896ee788a6f4f68172981d544b050bd
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-15 15:27:15 +0900

    인벤토리 막힘

--- GameItemException/Program.cs
+++ GameItemException/Program.cs
@@ -3,3 +3,29 @@ using System.Collections.Generic;
 
 // README.md를 읽고 코드를 작성하세요.
 Console.WriteLine("코드를 작성하세요.");
+
+
+
+class Inventory
+{
+    public List<string> items = new List<string>();
+
+    public int maxCapacity {  get; private set; }
+
+    public Inventory(int maxcapacity)
+    {
+        this.maxCapacity = maxcapacity;
+    }
+
+    public void AddItem(string itemname)
+    {
+        try
+        {
+            items.Add(itemname);
+        }
+        catch(InventoryFullException ex) when()
+        {
+            
+        }
+    }
+}
